b3f672c38b53c598c0ef17b914dddc50b777cf31
AWard59 <iamsancho5990@hotmail.com>
2023-04-17 22:16:59 -0700

    feat: home & header components

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Welcome to SOL-Searchers</h1>
+      <p>Join the decentralized dating revolution today</p>
+      <button>Get started</button>
+    </div>
+  )
+}
+
+export default Home
