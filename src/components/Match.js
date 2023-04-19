8675c7d0408a2750437b095dc90e4d39eecb7c02
AWard59 <iamsancho5990@hotmail.com>
2023-04-18 21:49:28 -0700

    feat: match & profile components

---
+++ src/components/Match.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const Match = () => {
+  const [matches, setMatches] = useState([])
+
+  useEffect(() => {
+    const getMatches = async () => {
+      const res = await axios.get('../api/matches');
+      setMatches(res.data)
+    }
+    getMatches()
+  }, [])
+
+  return (
+    <div>
+      <h1>Potential Matches</h1>
+      {matches.map((match) => {
+        <div key={match.id}>
+          <h2>{match.name}</h2>
+          <p>Age: {match.age}</p>
+          <p>Location: {match.location}</p>
+        </div>
+      })}
+    </div>
+  )
+}
+
+export default Match
